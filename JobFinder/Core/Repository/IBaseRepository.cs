dbf39121678e962a9c7fc2f585a758687078706e
nqanpreciofishbone <an.nguyen.quang@preciofishbone.se>
2024-10-01 17:52:14 +0700

    Update: Add IBaseRepository, BaseRepository

---
+++ JobFinder/Core/Repository/IBaseRepository.cs
@@ -0,0 +1,23 @@
+using JobFinder.Core.Entity;
+using System.Linq.Expressions;
+
+namespace JobFinder.Core.Repository
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetAsync(Guid id);
+
+        Task<int> AddRangeAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> GetFirstAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task<TEntity> DeleteAsync(TEntity entity);
+
+        Task<List<TEntity>> GetByPagination(int skip, int limit, Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null);
+    }
+}
