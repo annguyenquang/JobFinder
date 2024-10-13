448cc77e6423ac93dbc3b7d4d9493d02da399a75
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-13 16:11:08 +0700

    Update: Add company fields

--- JobFinder/Core/Entity/Company.cs
+++ JobFinder/Core/Entity/Company.cs
@@ -1,12 +1,20 @@
-﻿namespace JobFinder.Core.Entity
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JobFinder.Core.Entity
 {
+    [Index(nameof(Slug), IsUnique = true)] 
     public class Company : BaseEntity, ICredentialEntity
     {
         public Guid AccountId { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string EmailContact { get; set; } = string.Empty;
         public string PhoneContact { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string Website { get; set; } = string.Empty;
+        public int? EmployeeCount { get; set; } 
+        public string Slug { get;set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public string? Website { get; set; } = string.Empty;
+        public string? Industry { get; set; } = string.Empty;
+        public string Nation { get; set; } = string.Empty;
         public Account Account { get; set; } = null!;
         public IEnumerable<Position> Positions { get; set; } = new List<Position>();
         public DateTime? CreatedAt { get; set; }
---
+++ JobFinder/Migrations/20241013090658_AddCompanyFields.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: new Guid("17763d5d-f84c-423f-aacb-a35b687b297a"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Website",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeCount",
+                table: "Companies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Industry",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nation",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Companies",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "Email", "Password", "Phone", "UpdatedAt", "UpdatedBy", "Username" },
+                values: new object[] { new Guid("edc7a930-0226-42fe-aa78-c161711e8785"), null, null, "admin@gmail.com", "Admin", "0123456789", null, null, "Admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_Slug",
+                table: "Companies",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_Slug",
+                table: "Companies");
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: new Guid("edc7a930-0226-42fe-aa78-c161711e8785"));
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeCount",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Industry",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Nation",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Website",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "Email", "Password", "Phone", "UpdatedAt", "UpdatedBy", "Username" },
+                values: new object[] { new Guid("17763d5d-f84c-423f-aacb-a35b687b297a"), null, null, "admin@gmail.com", "Admin", "0123456789", null, null, "Admin" });
+        }
+    }
+}
