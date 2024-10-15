9ca9ddd8a27b5b911e0019c7c28d3835da5ce062
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-15 22:18:55 +0700

    Add pagination infor for GetAllPositions response

--- JobFinder/Service/PositionService/PositionService.cs
+++ JobFinder/Service/PositionService/PositionService.cs
@@ -4,11 +4,16 @@ using JobFinder.Core.Repository;
 using JobFinder.Model;
 using JobFinder.Model.Utils.Fetching;
 using JobFinder.Model.Utils.Fetching.Filters;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace JobFinder.Service
 {
+    
     public class PositionService(IPositionRepository _positionRepository, IMapper _mapper) : IPositionService
     {
+        private const int DEFAULT_PAGENUMBER = 1;
+        private const int DEFAULT_PAGESIZE = 10;
+        private const int MAX_PAGESIZE = 30;
         public async Task<CreatePositionReponseModel> CreatePositionAsync(CreatePositionModel position)
         {
             var positionEntity = _mapper.Map<Position>(position);
@@ -16,12 +21,18 @@ namespace JobFinder.Service
             return _mapper.Map<CreatePositionReponseModel>(entity);
         }
 
-        public async Task<List<PositionModel>> GetAllPositionAsync(PositionFilter filter, Order order, Pagination pagination)
+        public async Task<ListResponseModel<PositionModel>> GetAllPositionAsync(PositionFilter filter, Order order, Pagination pagination)
         {
-            var entities = await _positionRepository.GetAllAsync(filter, order, pagination);
-            return _mapper.Map<List<PositionModel>>(entities);
-        }
-
+            Pagination returnPagination = validatePagination(pagination);
+            var entities = await _positionRepository.GetAllAsListModelAsync(filter, order, returnPagination);
+            var models = _mapper.Map<List<PositionModel>>(entities.Data);
+            return new ListResponseModel<PositionModel>()
+            {
+                Data = models,
+                Total = entities.Total,
+                Pagination = returnPagination
+            };
+        }   
         public async Task<PositionModel> GetPositionAsync(Guid id)
         {
             var entity = await _positionRepository.GetAsync(id);
@@ -32,6 +43,27 @@ namespace JobFinder.Service
             Position newPosition = _mapper.Map<Position>(newPositionModel);
             var res = await _positionRepository.UpdateAsync(id, newPosition);
             return _mapper.Map<UpdatePositionReponseModel>(res);
+        }
+
+        private Pagination validatePagination(Pagination pagination)
+        {
+            if (pagination == null)
+            {
+                return new Pagination
+                {
+                    Page = DEFAULT_PAGENUMBER,
+                    PageSize = DEFAULT_PAGESIZE
+                };
+            }
+            if (pagination.Page < 1)
+            {
+                pagination.Page = DEFAULT_PAGENUMBER;
+            }
+            if (pagination.PageSize < 1 || pagination.PageSize > MAX_PAGESIZE)
+            {
+                pagination.PageSize = DEFAULT_PAGESIZE;
+            }
+            return pagination;
         }
     }
 }
