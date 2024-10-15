acce42c0477f901d074a1bbef7d82aa1a0f57d9b
nqanpreciofishbone <an.nguyen.quang@preciofishbone.se>
2024-10-15 15:56:50 +0700

    Make position close date nullable

--- JobFinder/Core/Entity/Position.cs
+++ JobFinder/Core/Entity/Position.cs
@@ -10,7 +10,7 @@ namespace JobFinder.Core.Entity
         public string Description { get; set; } = string.Empty;
         public double? Salary { get; set; }
         public PositionStatus Status { get; set; }
-        public DateTime CloseDate { get; set; }
+        public DateTime? CloseDate { get; set; }
         public WorkArrangement? WorkArrangement { get; set; }
         public CommitmentType? CommitmentType { get; set; }
         /// Location 
---
+++ JobFinder/Migrations/20241015084552_MakePositionCloseDateNullable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePositionCloseDateNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: new Guid("fdd48c46-5824-4565-af02-8470b0213166"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CloseDate",
+                table: "Positions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "Email", "Password", "Phone", "UpdatedAt", "UpdatedBy", "Username" },
+                values: new object[] { new Guid("784097f0-dc2c-433c-9841-d1a759933ad4"), null, null, "admin@gmail.com", "Admin", "0123456789", null, null, "Admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: new Guid("784097f0-dc2c-433c-9841-d1a759933ad4"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CloseDate",
+                table: "Positions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "Email", "Password", "Phone", "UpdatedAt", "UpdatedBy", "Username" },
+                values: new object[] { new Guid("fdd48c46-5824-4565-af02-8470b0213166"), null, null, "admin@gmail.com", "Admin", "0123456789", null, null, "Admin" });
+        }
+    }
+}
