8de83463bbc7319273296ff9a290c0de11e169df
nqanpreciofishbone <an.nguyen.quang@preciofishbone.se>
2024-10-15 15:59:42 +0700

    Update: Update CreatePositionModel, PositionModel, UpdatePositionModel

--- JobFinder/Model/Position/CreatePositionModel.cs
+++ JobFinder/Model/Position/CreatePositionModel.cs
@@ -6,9 +6,20 @@ namespace JobFinder.Model
     {
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public double Salary { get; set; }
-        public PositionStatus Status { get; set; }
-        public DateTime CloseDate { get; set; }
+        public double? Salary { get; set; }
+        public PositionStatus? Status { get; set; } = PositionStatus.Open;
+        public DateTime? CloseDate { get; set; }
+        public WorkArrangement? WorkArrangement { get; set; }
+        public CommitmentType? CommitmentType { get; set; }
+        /// Location 
+        public int? ProvinceId { get; set; }
+        public int? DistrictId { get; set; }
+        /// Requirements
+        public int? MinAgeRequirement { get; set; }
+        public int? MaxAgeRequirement { get; set; }
+        public GenderRequirement? GenderRequirement { get; set; }
+        public EducationLevelRequirement? EducationLevelRequirement { get; set; }
+        public WorkExperienceRequirement? WorkExperienceRequirement { get; set; }
         public Guid CompanyId { get; set; }
     }
     public class CreatePositionReponseModel : BaseResponseModel { }
--- JobFinder/Model/Position/PositionModel.cs
+++ JobFinder/Model/Position/PositionModel.cs
@@ -8,7 +8,7 @@ namespace JobFinder.Model
         public string Description { get; set; } = string.Empty;
         public double? Salary { get; set; }
         public PositionStatus Status { get; set; }
-        public DateTime CloseDate { get; set; }
+        public DateTime? CloseDate { get; set; }
         public WorkArrangement? WorkArrangement { get; set; }
         public CommitmentType? CommitmentType { get; set; }
         public int? ProvinceId { get; set; }
