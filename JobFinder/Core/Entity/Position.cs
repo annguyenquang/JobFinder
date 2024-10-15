77cac6d00fc0d5619555d612888c2c275ac027b8
nqanpreciofishbone <an.nguyen.quang@preciofishbone.se>
2024-10-15 11:39:05 +0700

    Fix Position entity, get risk of object property

--- JobFinder/Core/Entity/Position.cs
+++ JobFinder/Core/Entity/Position.cs
@@ -2,8 +2,8 @@
 
 namespace JobFinder.Core.Entity
 {
-   
-    
+
+
     public class Position : BaseEntity, ICredentialEntity
     {
         public string Title { get; set; } = string.Empty;
@@ -11,16 +11,22 @@ namespace JobFinder.Core.Entity
         public double? Salary { get; set; }
         public PositionStatus Status { get; set; }
         public DateTime CloseDate { get; set; }
-        public Location Location { get; set; } = null!;
         public WorkArrangement? WorkArrangement { get; set; }
         public CommitmentType? CommitmentType { get; set; }
-        public JobRequirement? Requirements { get; set; }
-       
+        /// Location 
+        public int? ProvinceId { get; set; }
+        public int? DistrictId { get; set; }
+        /// Requirements
+        public int? MinAgeRequirement { get; set; }
+        public int? MaxAgeRequirement { get; set; }
+        public GenderRequirement? GenderRequirement { get; set; }
+        public EducationLevelRequirement? EducationLevelRequirement { get; set; }
+        public WorkExperienceRequirement? WorkExperienceRequirement { get; set; }
+        /// Navigations 
         public Guid CompanyId { get; set; }
         public Company Company { get; set; } = null!;
-       
         public IEnumerable<PositionApplication> PositionApplications { get; set; } = new List<PositionApplication>();
-        
+        /// Creditial props 
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public Guid? CreatedBy { get; set; }
--- JobFinder/Model/Position/SubProps/Location.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JobFinder.Model
-{
-    public record Location
-    {
-        public int? ProvinceId { get; set; }
-        public int? DistrictId { get; set; }
-    }
-}
