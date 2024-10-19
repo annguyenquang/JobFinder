7cacf0bd08f7aa1e0a2a105b0944c60fa6344a11
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-19 16:05:14 +0700

    Update Company Model, CreateCompanyModel

--- JobFinder/Model/Company/CompanyModel.cs
+++ JobFinder/Model/Company/CompanyModel.cs
@@ -3,9 +3,14 @@ namespace JobFinder.Model
 {
     public class CompanyModel : BaseResponseModel
     {
+        public string Name { get; set; } = string.Empty;
         public string EmailContact { get; set; } = string.Empty;
         public string PhoneContact { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string Website { get; set; } = string.Empty;
+        public int? EmployeeCount { get; set; } 
+        public string Slug { get;set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public string? Website { get; set; } = string.Empty;
+        public string? Industry { get; set; } = string.Empty;
+        public string Nation { get; set; } = string.Empty;
     }
 }
--- JobFinder/Model/Company/CreateCompanyModel.cs
+++ JobFinder/Model/Company/CreateCompanyModel.cs
@@ -7,8 +7,10 @@ namespace JobFinder.Model
         public Guid AccountId { get; set; }
         public string EmailContact { get; set; } = string.Empty;
         public string PhoneContact { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
-        public string Website { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public string? Website { get; set; } = string.Empty;
+        public string? Industry { get; set; } = string.Empty;    
+        public int? EmployeeCount { get; set; } 
 
     }
     public class CreateCompanyResponseModel : BaseResponseModel { }
