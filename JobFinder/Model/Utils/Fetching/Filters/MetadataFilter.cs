890c96fb83728c321df480546bc78b1b840607bf
nqanpreciofishbone <an.nguyen.quang@preciofishbone.se>
2024-10-22 16:07:20 +0700

    feature: add Metadata service

---
+++ JobFinder/Model/Utils/Fetching/Filters/MetadataFilter.cs
@@ -0,0 +1,18 @@
+﻿using JobFinder.Core.Entity;
+
+namespace JobFinder.Model.Utils.Fetching.Filters
+{
+    public class MetadataFilter : IFilter<Metadata>
+    {
+        public MetadataType? MetadataType { get; set; }
+
+        public IQueryable<Metadata> filters(IQueryable<Metadata> queryable)
+        {
+            if(MetadataType.HasValue)
+            {
+                queryable = queryable.Where(x => x.Type == MetadataType);
+            }
+            return queryable;
+        }
+    }
+}
