d3d26a942818422eb91da12dbc732a31e3e165c9
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-28 23:28:42 +0700

    Add Cors Policies

--- JobFinder/Model/Utils/AppSettings/AppSettings.cs
+++ JobFinder/Model/Utils/AppSettings/AppSettings.cs
@@ -3,6 +3,7 @@
     public class AppSettings
     {
         public const string AppSetting = "AppSettings";
+        public string ClientUrl { get; set; } = string.Empty;
         public string AZURE_STORAGE_CONNECTION_STRING { get; set; } = string.Empty;
         public string JwtSecretKey { get; set; } = string.Empty;
     }
