091338413641130a30c94ab85c5dfdcbbbd6dc43
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-30 10:48:02 +0700

    Clean CreateCompanyModel.cs

--- JobFinder/Model/Company/CreateCompanyModel.cs
+++ JobFinder/Model/Company/CreateCompanyModel.cs
@@ -1,22 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using JobFinder.Core.Entity;
 
-namespace JobFinder.Model
+namespace JobFinder.Model;
+public class CreateCompanyModel
 {
-    public class CreateCompanyModel
-    {
-        public Guid AccountId { get; set; }
-        public string EmailContact { get; set; } = string.Empty;
-        public string PhoneContact { get; set; } = string.Empty;
-        [Range(0, 100, ErrorMessage = "The field {0} must be a number between 0 and 96.")]
-        public int ProvinceId { get; set; }
-        [Range(0, 1000, ErrorMessage = "The field {0} must be a number between 0 and 1000.")]
-        public int DistrictId { get; set; }
-        public string? Address { get; set; } = string.Empty;
-        public string? Website { get; set; } = string.Empty;
-        public string? Industry { get; set; } = string.Empty;    
-        public int? EmployeeCount { get; set; } 
+    public Guid AccountId { get; set; }
+    public string EmailContact { get; set; } = string.Empty;
+    public string PhoneContact { get; set; } = string.Empty;
 
-    }
-    public class CreateCompanyResponseModel : BaseResponseModel { }
+    [Range(0, 100, ErrorMessage = "The field {0} must be a number between 0 and 96.")]
+    public int ProvinceId { get; set; }
+
+    [Range(0, 1000, ErrorMessage = "The field {0} must be a number between 0 and 1000.")]
+    public int DistrictId { get; set; }
+
+    public string? Address { get; set; } = string.Empty;
+    public string? Website { get; set; } = string.Empty;
+    public string? Industry { get; set; } = string.Empty;
+    public int? EmployeeCount { get; set; }
 }
+
+public class CreateCompanyResponseModel : BaseResponseModel;
