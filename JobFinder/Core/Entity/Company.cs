c9ef67de7f092cd92e7585b9c064465e8ae24366
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-10-30 10:25:37 +0700

    Add ProvinceId, DistrictId to Company Entity

--- JobFinder/Core/Entity/Company.cs
+++ JobFinder/Core/Entity/Company.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobFinder.Core.Entity
 {
@@ -11,6 +12,10 @@ namespace JobFinder.Core.Entity
         public string PhoneContact { get; set; } = string.Empty;
         public int? EmployeeCount { get; set; } 
         public string Slug { get;set; } = string.Empty;
+        [Range(0, 100, ErrorMessage = "The field {0} must be a number between 0 and 96.")]
+        public int ProvinceId { get; set; }
+        [Range(0, 1000, ErrorMessage = "The field {0} must be a number between 0 and 1000.")]
+        public int DistrictId { get; set; }
         public string? Address { get; set; } = string.Empty;
         public string? Website { get; set; } = string.Empty;
         public string? Industry { get; set; } = string.Empty;
