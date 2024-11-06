fd5b49c36ddda9174ac0a421fc6bb3ff8a279127
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-06 20:03:24 +0700

    update: add Id to AccountModel.cs

--- JobFinder/Model/Account/AccountModel.cs
+++ JobFinder/Model/Account/AccountModel.cs
@@ -3,10 +3,11 @@
 namespace JobFinder.Model;
 [JsonDerivedType(typeof(CompanyLoginResponseModel))]
 [JsonDerivedType(typeof(UserLoginReponseModel))]
-public abstract class AccountModel
+public abstract class AccountModel: IBaseResponseModel
 {
     public string Username { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string? AccessToken { get; set; }
+    public Guid Id { get; set; }
 }
