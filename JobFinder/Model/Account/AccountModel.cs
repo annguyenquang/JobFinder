2469a8d5e0021ad894a89442031aa47cf6ce525d
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-06 13:54:01 +0700

    update: make AccountModel AccessToken nullable

--- JobFinder/Model/Account/AccountModel.cs
+++ JobFinder/Model/Account/AccountModel.cs
@@ -1,10 +1,9 @@
-﻿namespace JobFinder.Model
+﻿namespace JobFinder.Model;
+
+public class AccountModel
 {
-    public class AccountModel
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Phone { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string AccessToken { get; set; } = string.Empty;
-    }
+    public string Username { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? AccessToken { get; set; }
 }
