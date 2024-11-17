d91b7dca0f590c4cabd394f88fd26ff5c8adcf28
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-18 01:55:09 +0700

    chore: update LatestSearchKeyword -> LatestSearchKeywords

--- JobFinder/Model/Account/SuggestibleUserModel.cs
+++ JobFinder/Model/Account/SuggestibleUserModel.cs
@@ -3,5 +3,5 @@ namespace JobFinder.Model;
 public class SuggestibleUserModel 
 {
     public IEnumerable<string> Skills { get; set; } = Enumerable.Empty<string>();
-    public IEnumerable<string> LatestSearchKeyword { get; set; } = new List<string>();
+    public IEnumerable<string> LatestSearchKeywords { get; set; } = new List<string>();
 }
