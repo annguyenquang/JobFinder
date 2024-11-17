28b7e7337efce2ba2a7ba027b396b38143a5d74c
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-17 12:03:37 +0700

    chore: clean

--- JobFinder/Controllers/MetadataController.cs
+++ JobFinder/Controllers/MetadataController.cs
@@ -6,12 +6,11 @@ namespace JobFinder.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class MetadataController(IMetadataService _metadataService, IJobSuggestionService _jobSuggestionService) : ControllerBase
+    public class MetadataController(IMetadataService _metadataService) : ControllerBase
     {
         [HttpGet]
         public async Task<ApiResult<ListResponseModel<MetadataModel>>> GetMetadataByPagination([FromQuery] GetMetadataByPaginationParams param)
         {
-            _jobSuggestionService.GetJobSuggestionWithExplanation(new UserModel(), new List<JobModel>());
             var metadataListResponseModel = await _metadataService.GetAllMetadataAsync(param.Filter, param.Order, param.Pagination);
             return ApiResult<ListResponseModel<MetadataModel>>.Success(metadataListResponseModel);
         }
