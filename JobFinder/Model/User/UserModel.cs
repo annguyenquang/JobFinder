d706a33b5393cd02f11e5eb628241e7c72192c52
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-11-18 21:07:53 +0700

    update: update user model

--- JobFinder/Model/User/UserModel.cs
+++ JobFinder/Model/User/UserModel.cs
@@ -5,6 +5,13 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateOnly DateOfBirth { get; set; }
+        public IEnumerable<string> Skills { get; set; } = new List<string>();
+        public string SelfDescription { get; set; } = string.Empty;
+        public IEnumerable<Certification> Certifications { get; set; } = new List<Certification>();
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
         public Guid Id { get; set; }
     }
 }
