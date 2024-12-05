5504d932f99e0b828425369131c6a5bd8b6ac716
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-05 10:53:49 +0700

    update prompt to handle case empty job

--- JobFinder/Service/JobSuggestionService/JobSuggestionService.cs
+++ JobFinder/Service/JobSuggestionService/JobSuggestionService.cs
@@ -62,7 +62,7 @@ public sealed class JobSuggestionService(IGeminiClient _geminiClient, IJobServic
                                                        Female ("7b5f69cb-5996-4a57-b9c6-9fee2a791bf6")
                                                        Others ("12497687-64b8-4d8e-814a-b7d1d33d3aab")
                                                        Please evaluate these attributes for each job in the given list and return recommendations based on alignment with the user profile in the format specified. And the more suggestion, the more satisfy.
-                                                       Return at least one job.
+                                                       Return at least one job if the provided jobs is not empty, and if the provided jobs is empty return a empty array.
                            """
                 }
             ]
