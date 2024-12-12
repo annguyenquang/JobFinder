8c78b7953cd2de66324d5a74a117e782c0c79f0b
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-12 22:01:38 +0700

    update: add HasCoverLetter, Keyword to filter

--- JobFinder/Model/JobApplication/JobApplicationModel.cs
+++ JobFinder/Model/JobApplication/JobApplicationModel.cs
@@ -3,9 +3,10 @@
     public class JobApplicationModel
         : IBaseResponseModel
     {
-        public JobModel Job { get; set; }
-        public UserModel User { get; set; }
+        public Guid JobId { get; set; }
+        public Guid UserId { get; set; }
         public string CVLink { get; set; } = string.Empty;
+        public string CoverLetter { get; set; } = string.Empty;
 
         public Guid Id { get; set; }
     }
