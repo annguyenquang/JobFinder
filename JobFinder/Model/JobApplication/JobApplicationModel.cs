27c68d07c54b657f6016c6323562d302709c5eab
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-12-17 20:56:52 +0700

    update: add auditable properties to job application model

--- JobFinder/Model/JobApplication/JobApplicationModel.cs
+++ JobFinder/Model/JobApplication/JobApplicationModel.cs
@@ -1,13 +1,20 @@
-﻿namespace JobFinder.Model
+﻿using JobFinder.Core.Entity;
+
+namespace JobFinder.Model
 {
     public class JobApplicationModel
-        : IBaseResponseModel
+        : IBaseResponseModel, IAuditableEntity
     {
         public Guid JobId { get; set; }
         public Guid UserId { get; set; }
         public string CVLink { get; set; } = string.Empty;
         public string CoverLetter { get; set; } = string.Empty;
 
-        public Guid Id { get; set; }
+        public Guid Id { get; set; }    
+        
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
     }
 }
